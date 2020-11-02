7cdc8c7421cb6cf679ea404f51939406538ab3cd
tonystratum <str4tum@gmail.com>
2020-11-02 13:33:50 +0200

    New recursion logic, tab symbol may be chosen

--- jsontree.py
+++ jsontree.py
@@ -1,30 +1,26 @@
-def jsontree(json, key=None, index=0, dct=False):
-    if not isinstance(json, dict) and not isinstance(json, list):
-        if key is None:
-            print(''.join(['\t' for i in range(index)]),
-                  f'"{json}",')
-        else:
-            print(''.join(['\t' for i in range(index)]),
-                  f'"{key}": "{json}",')
-        return True
+def get_tabs(n, symbol):
+    return ''.join([symbol for i in range(n)])
 
-    if isinstance(json, list):
-        if not dct:
-            print(''.join(['\t' for i in range(index)]), f'[')
-        else:
-            if key is None:
-                print(''.join(['\t' for i in range(index)]), '{')
-            else:
-                print(''.join(['\t' for i in range(index)]), f'"{key}": {"{"}')
 
-        for item in json:
-            if isinstance(item, tuple):
-                jsontree(item[1], key=item[0], index=index + 1)
-            else:
-                jsontree(item, index=index + 1)
-        if not dct:
-            print(''.join(['\t' for i in range(index)]), ']')
+def jsontree(json, symbol='\t', depth=0):
+    tabs = get_tabs(depth, symbol)
 
-    elif isinstance(json, dict):
-        jsontree(list(json.items()), key=key, index=index, dct=True)
-        print(''.join(['\t' for i in range(index)]), '},')
+    if depth == 0:
+        print(jsontree(json, symbol, depth + 1))
+
+    if isinstance(json, dict):
+        items = ['{' + '\n']
+        for k, v in json.items():
+            items.append('{}"{}": {},\n'.format(tabs, k, jsontree(v, symbol, depth + 1)))
+        items.append(get_tabs(depth - 1, symbol) + '}')
+        return ''.join(items)
+    elif isinstance(json, list):
+        items = ['[' + '\n']
+        for v in json:
+            items.append('{}{},\n'.format(tabs, jsontree(v, symbol, depth + 1)))
+        items.append(get_tabs(depth - 1, symbol) + ']')
+        return ''.join(items)
+    else:
+        if isinstance(json, str):
+            return '"{}"'.format(json)
+        return str(json)
